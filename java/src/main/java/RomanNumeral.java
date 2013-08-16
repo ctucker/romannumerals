bbd1d2b22e2af3ca9e3195f60dba98388e900a06
Chris Tucker <chris.tucker@service-now.com>
2013-08-15 23:02:42 -0700

    Recursive version.
    Uses Guava libs to simplify map management, easy to sub out if desired

--- java/src/main/java/RomanNumeral.java
+++ java/src/main/java/RomanNumeral.java
@@ -1,2 +1,42 @@
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSortedMap;
+
 public class RomanNumeral {
+
+	private final static ImmutableSortedMap<Integer, String> numeralConversions =
+			ImmutableSortedMap.<Integer, String>reverseOrder()
+			                  .put(1000, "M")
+			                  .put(900, "CM")
+			                  .put(500, "D")
+			                  .put(400, "CD")
+			                  .put(100, "C")
+			                  .put(90, "XC")
+			                  .put(50, "L")
+			                  .put(40, "XL")
+			                  .put(10, "X")
+			                  .put(9, "IX")
+			                  .put(5, "V")
+			                  .put(4, "IV")
+			                  .put(1, "I")
+			                  .build();
+
+	private final int decimalValue;
+
+	public RomanNumeral(int decimalValue) {
+		Preconditions.checkArgument(decimalValue > 0);
+		this.decimalValue = decimalValue;
+	}
+
+	public String asString() {
+		return buildString(decimalValue);
+	}
+
+	private String buildString(int remaining) {
+		for (Integer decimal : numeralConversions.keySet()) {
+			if (remaining >= decimal) {
+				return numeralConversions.get(decimal) + buildString(remaining - decimal);
+			}
+		}
+		return "";
+	}
 }
--- java/src/test/java/IntegerToRomanNumeralTest.java
+++ java/src/test/java/IntegerToRomanNumeralTest.java
@@ -7,8 +7,123 @@ import org.junit.Test;
 public class IntegerToRomanNumeralTest {
 
 	@Test
-	public void canary() {
-		assertThat(true, is(equalTo(true)));
+	public void decimal1IsRomanI() {
+		verifyConversion(1, "I");
 	}
+
+	@Test
+	public void decimal2IsRomanII() {
+		verifyConversion(2, "II");
+	}
+
+	@Test
+	public void decimal5IsRomanV() {
+		verifyConversion(5, "V");
+	}
+
+	@Test
+	public void decimal6UsesAdditiveCaseToGetVI() {
+		verifyConversion(6, "VI");
+	}
+
+	@Test
+	public void decimal4UsesSubtractiveCaseToGetIV() {
+		verifyConversion(4, "IV");
+	}
+
+	@Test
+	public void decimal10IsRomanX() {
+		verifyConversion(10, "X");
+	}
+
+	@Test
+	public void decimal12UsesAdditiveCaseToGetXII() {
+		verifyConversion(12, "XII");
+	}
+
+	@Test
+	public void decimal20UsesRepetitiveCaseToGetXX() {
+		verifyConversion(20, "XX");
+	}
+
+	@Test
+	public void decimal9UsesSubtractiveCaseToGetIX() {
+		verifyConversion(9, "IX");
+	}
+
+	@Test
+	public void decimal14UsesAdditiveOnTenAndSubtractiveOn4ToGetXIV() {
+		verifyConversion(14, "XIV");
+	}
+
+	@Test
+	public void decimala50IsRomanL() {
+		verifyConversion(50, "L");
+	}
+
+	@Test
+	public void decimal_40_uses_subtractive_case_to_get_XL() {
+		verifyConversion(40, "XL");
+	}
+
+	@Test
+	public void decimal_44_uses_subtractive_additive_and_subtractive_to_get_XLIV() {
+		verifyConversion(44, "XLIV");
+	}
+
+	@Test
+	public void decimal_100_is_roman_C() {
+		verifyConversion(100, "C");
+	}
+
+	@Test
+	public void decimal_90_uses_subtractive_case_to_get_XC() {
+		verifyConversion(90, "XC");
+	}
+
+	@Test
+	public void decimal_500_is_roman_D() {
+		verifyConversion(500, "D");
+	}
+
+	@Test
+	public void decimal_400_uses_subtractive_case_to_get_CD() {
+		verifyConversion(400, "CD");
+	}
+
+	@Test
+	public void decimal_1000_is_roman_M() {
+		verifyConversion(1000, "M");
+	}
+
+	@Test
+	public void decimal_900_uses_subtractive_case_to_get_CM() {
+		verifyConversion(900, "CM");
+	}
+
+	@Test
+	public void simple_repetitions_through_all_concatenate_together() {
+		verifyConversion(3786, "MMMDCCLXXXVI");
+	}
+
+	@Test
+	public void multiple_repeated_subtractions_concatenate_together() {
+		verifyConversion(944, "CMXLIV");
+	}
+
+	@Test
+	public void mixture_of_additions_and_subtractions_concatenate_together() {
+		verifyConversion(1951, "MCMLI");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void conversion_of_non_integer_throws_IAE() {
+		verifyConversion(-1, "ERROR");
+	}
+
+	private void verifyConversion(int decimal, String expected) {
+		assertThat(new RomanNumeral(decimal).asString(), is(equalTo(expected)));
+	}
+
 
 }
