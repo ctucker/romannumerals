f02674ff82b9657879e3b8bb4dd81f07fe7b59f9
Chris Tucker <chris.tucker@service-now.com>
2013-10-08 12:10:07 -0700

    Partial js implementation

--- js/spec/integerToNumeralsSpec.js
+++ js/spec/integerToNumeralsSpec.js
@@ -4,6 +4,18 @@ describe('Integer to Roman Numeral conversion', function() {
 
 	it('should convert 1 to the letter I', function() {
 		expect(converter.integerToNumeral(1)).toBe("I");
-	})
+	});
+
+	it('should convert 3 to the sequence III', function() {
+		expect(converter.integerToNumeral(2)).toBe('II');
+	});
+
+	it('should convert 5 to the letter V', function() {
+		expect(converter.integerToNumeral(5)).toBe('V');
+	});
+
+	it('should convert 6 to the sequence VI', function() {
+		expect(converter.integerToNumeral(6)).toBe('VI');
+	});
 
 });
--- js/src/RomanNumerals.js
+++ js/src/RomanNumerals.js
@@ -1,4 +1,23 @@
-function integerToNumeral() {
-	return void(0);
+var ROMAN_ONE = 'I';
+
+var conversions = [
+	{ 1 : 'I' },
+	{ 5 : 'V' }
+];
+
+
+function integerToNumeral(intValue) {
+	if (intValue === 0) {
+		return '';
+	}
+
+	if (intValue >= 5) {
+		return 'V' + integerToNumeral(intValue - 5);
+	}
+
+	return ROMAN_ONE + integerToNumeral(intValue - 1);
 }
+
+
+
 exports['integerToNumeral'] = integerToNumeral;
